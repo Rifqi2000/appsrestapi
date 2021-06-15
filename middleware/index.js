2691d3af948d989578557c55516c6c911ca48b6d
Rifqi2000 <rifqimulyakiswanto@gmail.com>
2021-06-15 09:53:16 +0700

    Login

--- middleware/index.js
+++ middleware/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var auth = require('./auth');
 var router = express.Router();
 
-
+// register registration menu
 router.post('/api/v1/register',auth.register);
+router.post('/api/v1/login',auth.login);
 
 
 module.exports = router;
